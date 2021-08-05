96660fd8283c35398a69130bb814b20a4cd6f6b1
ATkingma <keizerxjwz4@gmail.com>
2021-08-05 14:57:52 +0200

    prefabs

--- Regnarok/Assets/Scripts/map gen/EnviromentSpawner.cs
+++ Regnarok/Assets/Scripts/map gen/EnviromentSpawner.cs
@@ -1,61 +1,149 @@
-//using System.Collections;
-//using UnityEngine;
-//public class EnviromentSpawner : MonoBehaviour
-//{
-//    private void MapEmbellishments(float noiseHeight, int posX, int posZ)
-//    {
-//        // Set min and max height of map for color gradient
-//        if (noiseHeight > maxTerrainheight)
-//            maxTerrainheight = noiseHeight;
-//        if (noiseHeight < minTerrainheight)
-//            minTerrainheight = noiseHeight;
+using System.Collections;
+using UnityEngine;
+public class EnviromentSpawner : MonoBehaviour
+{
+    public GameObject tree;
+    public float minTreeSize;
+    public float maxTreeSize;
+    public Texture2D noiseImage;
+    public float forestSize;
+    public float treeDensity;
 
-//        if (System.Math.Abs(lastNoiseHeight - noiseHeight) < 25)
-//        {
-//            // Generate trees on vertices and times by scale of map
-//            var bottomOfTile = posX * MESH_SCALE - 25;
-//            var topOfTile = posX * MESH_SCALE + 25;
-//            var rightOfTile = posZ * MESH_SCALE - 25;
-//            var leftOfTile = posZ * MESH_SCALE + 25;
+    private float baseDensity = 5.0f;
 
-//            var spawnAboveTerrainBy = noiseHeight + 25;
-//            if (noiseHeight > minTreeNoiseHeight && noiseHeight < maxTreeNoiseHeight)
-//            {
-//                for (var i = 0; i < 4; i++)
-//                {
-//                    if (Random.Range(1, 4) == 1)
-//                    {
-//                        GameObject objectToSpawn;
-//                        if (Random.Range(1, 5) == 1)
-//                        {
-//                            objectToSpawn = rocks[Random.Range(0, rocks.Length)];
-//                        }
-//                        else
-//                        {
-//                            objectToSpawn = trees[Random.Range(0, trees.Length)];
-//                        }
+    // Use this for initialization
+    void Start()
+    {
+        Generate();
+    }
 
-//                        switch (i)
-//                        {
-//                            case 0:
-//                                Instantiate(objectToSpawn, new Vector3(topOfTile, spawnAboveTerrainBy, leftOfTile), Quaternion.identity);
-//                                break;
-//                            case 1:
-//                                Instantiate(objectToSpawn, new Vector3(topOfTile, spawnAboveTerrainBy, rightOfTile), Quaternion.identity);
-//                                break;
-//                            case 2:
-//                                Instantiate(objectToSpawn, new Vector3(bottomOfTile, spawnAboveTerrainBy, leftOfTile), Quaternion.identity);
-//                                break;
-//                            case 3:
-//                                Instantiate(objectToSpawn, new Vector3(bottomOfTile, spawnAboveTerrainBy, rightOfTile), Quaternion.identity);
-//                                break;
-//                            default:
-//                                break;
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//        lastNoiseHeight = noiseHeight;
-//    }
-//}
+    public void Generate()
+    {
+
+        for (int y = 0; y < forestSize; y++)
+        {
+
+            for (int x = 0; x < forestSize; x++)
+            {
+
+                float chance = noiseImage.GetPixel(x, y).r / (baseDensity / treeDensity);
+
+                if (ShouldPlaceTree(chance))
+                {
+                    float size = Random.Range(minTreeSize, maxTreeSize);
+
+                    GameObject newTree = Instantiate(tree);
+                    newTree.transform.localScale = Vector3.one * size;
+                    newTree.transform.position = new Vector3(x, 0, y);
+                    newTree.transform.parent = transform;
+                }
+            }
+        }
+    }
+
+    //Returns true or false given some chance from 0 to 1
+    public bool ShouldPlaceTree(float chance)
+    {
+        if (Random.Range(0.0f, 1.0f) <= chance)
+        {
+            return true;
+        }
+        return false;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //public GameObject mesh;
+    //[Space(5)]
+    //public float maxTerrainheight;
+    //public float minTerrainheight;
+    //[Space(5)]
+    //public float lastNoiseHeight;
+
+    //[Header("Prefabs")]
+    //public GameObject[] trees;
+    //public GameObject[] rocks;
+    //public GameObject[] chests;
+    //private void MapEmbellishments(float noiseHeight, int posX, int posZ)
+    //{
+    //    Set min and max height of map for color gradient
+
+
+    //    if (System.Math.Abs(lastNoiseHeight - noiseHeight) < 25)
+    //        {
+    //            Generate trees on vertices and times by scale of map
+    //        var bottomOfTile = posX * mesh.transform.localScale.x - 25;
+    //            var topOfTile = posX * mesh.transform.localScale.x + 25;
+    //            var rightOfTile = posZ * mesh.transform.localScale.x - 25;
+    //            var leftOfTile = posZ * mesh.transform.localScale.x + 25;
+
+    //            var spawnAboveTerrainBy = noiseHeight + 25;
+
+    //            for (var i = 0; i < 4; i++)
+    //            {
+    //                if (Random.Range(1, 4) == 1)
+    //                {
+    //                    GameObject objectToSpawn;
+    //                    if (Random.Range(1, 5) == 1)
+    //                    {
+    //                        objectToSpawn = rocks[Random.Range(0, rocks.Length)];
+    //                    }
+    //                    else if (Random.Range(1, 5) == 2)
+    //                    {
+    //                        objectToSpawn = chests[Random.Range(0, chests.Length)];
+    //                    }
+    //                    else
+    //                    {
+    //                        objectToSpawn = trees[Random.Range(0, trees.Length)];
+    //                    }
+
+    //                    switch (i)
+    //                    {
+    //                        case 0:
+    //                            Instantiate(objectToSpawn, new Vector3(topOfTile, spawnAboveTerrainBy, leftOfTile), Quaternion.identity);
+    //                            break;
+    //                        case 1:
+    //                            Instantiate(objectToSpawn, new Vector3(topOfTile, spawnAboveTerrainBy, rightOfTile), Quaternion.identity);
+    //                            break;
+    //                        case 2:
+    //                            Instantiate(objectToSpawn, new Vector3(bottomOfTile, spawnAboveTerrainBy, leftOfTile), Quaternion.identity);
+    //                            break;
+    //                        case 3:
+    //                            Instantiate(objectToSpawn, new Vector3(bottomOfTile, spawnAboveTerrainBy, rightOfTile), Quaternion.identity);
+    //                            break;
+    //                        default:
+    //                            break;
+    //                    }
+    //                }
+    //            }
+
+    //        }
+    //    lastNoiseHeight = noiseHeight;
+    //}
--- Regnarok/Assets/Scripts/map gen/MapGenerator.cs
+++ Regnarok/Assets/Scripts/map gen/MapGenerator.cs
@@ -7,7 +7,6 @@ public class MapGenerator : MonoBehaviour
     // public
     public enum DrawMode{NoiseMap,ColourMap,Mesh, FalloffMap };
     public DrawMode drawMode;
-
     const int chuckSize = 241;
     [Range(0,6)]
     public int editorPrevieuwLOD;
@@ -24,11 +23,24 @@ public class MapGenerator : MonoBehaviour
     public bool autoUpdate;
     public bool useFallOffs;
     public TerrainType[] regions;
-	private void Awake()
+    [Space(5)]
+    public GameObject tree;
+    public float minTreeSize;
+    public float maxTreeSize;
+    private Texture2D noiseImage;
+    public float forestSize;
+    public float treeDensity;
+
+    private float baseDensity = 5.0f;
+    private void Awake()
 	{
         fallOffMap = FalloffGenerator.GenerateFalloffMap(chuckSize);
 	}
-	public void GenerateMap()
+    private void Start()
+    {
+        GenerateMap();
+    }
+    public void GenerateMap()
 	{
         float[,] noisemap = Noise.GenerateNoiseMap(chuckSize,chuckSize,seed,noiseScale,ocataves,presitance,lacunarity, offset);
 
@@ -74,7 +86,9 @@ public class MapGenerator : MonoBehaviour
         {
 			display.DrawTexture(TextureGenerator.TextureFromHeightMap(FalloffGenerator.GenerateFalloffMap(chuckSize)));
 		}
-	}
+
+     
+    }
 	private void OnValidate()
 	{
 		if (lacunarity < 1)
@@ -86,7 +100,56 @@ public class MapGenerator : MonoBehaviour
             ocataves = 0;
 		}
 	}
+
+    // Use this for initialization
+    public void GenerateTrees(Texture2D noiseValue)
+    {
+        print("3");
+        noiseImage = noiseValue;
+        for (int y = 0; y < forestSize; y++)
+        {
+            for (int x = 0; x < forestSize; x++)
+            {
+    
+                float chance = noiseImage.GetPixel(x, y).r / (baseDensity / treeDensity);
+                print(chance);
+                if (chance == 0)
+                {
+                    GenerateTrees(noiseValue);
+                    return;
+                }
+                if (ShouldPlaceTree(chance))
+                {
+                    print("1");
+                    float size = Random.Range(minTreeSize, maxTreeSize);
+
+                    GameObject newTree = Instantiate(tree);
+                    newTree.transform.localScale = Vector3.one * size;
+                    newTree.transform.position = new Vector3(x, 50, y);
+                }
+
+            }
+        }
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        display.DrawTexture(noiseValue);
+        print("5");
+    }
+
+    //Returns true or false given some chance from 0 to 1
+    public bool ShouldPlaceTree(float chance)
+    {
+        float randomNumb = Random.Range(0.0f, 1.0f);
+        print(randomNumb);
+        print("7");
+        if (randomNumb <= chance)
+        {
+            print("6");
+            return true;
+        }
+        return false;
+    }
 }
+
 [System.Serializable]
 public struct TerrainType {
 public string name;
