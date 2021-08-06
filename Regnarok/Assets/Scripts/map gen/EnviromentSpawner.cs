b2a80fb8b60e28325050ddd42bf01c540238e8ff
ATkingma <keizerxjwz4@gmail.com>
2021-08-06 20:24:21 +0200

    bomen stenen chest spawner

--- Regnarok/Assets/Scripts/map gen/EnviromentSpawner.cs
+++ Regnarok/Assets/Scripts/map gen/EnviromentSpawner.cs
@@ -2,148 +2,128 @@ using System.Collections;
 using UnityEngine;
 public class EnviromentSpawner : MonoBehaviour
 {
-    public GameObject tree;
-    public float minTreeSize;
-    public float maxTreeSize;
-    public Texture2D noiseImage;
-    public float forestSize;
-    public float treeDensity;
-
-    private float baseDensity = 5.0f;
-
-    // Use this for initialization
+    [Header("Prefabs")]
+    public GameObject firstObject;
+    public GameObject secondObject;
+    public GameObject thirdObject;
+    [Header("Settings")]
+    public Transform firstPos;
+    public Transform secondPos;
+    [Space(5)]
+    public bool randomRotFirst;
+    public bool randomRotSecond;
+    public bool randomRotThird;
+    [Space(5)]
+    public int firstAmount;
+    public int secondAmount;
+    public int thirdAmount;
+    public float spawnHeight = 20;
     void Start()
     {
         Generate();
     }
-
     public void Generate()
     {
-
-        for (int y = 0; y < forestSize; y++)
+        for (int i = 0; i < firstAmount; i++)
         {
-
-            for (int x = 0; x < forestSize; x++)
+            if (Chance())
             {
-
-                float chance = noiseImage.GetPixel(x, y).r / (baseDensity / treeDensity);
-
-                if (ShouldPlaceTree(chance))
+                Vector3 spawnPoint = new Vector3(Random.Range(firstPos.position.x, secondPos.position.x), spawnHeight, Random.Range(firstPos.position.z, secondPos.position.z));
+                Ray ray = new Ray(spawnPoint, -transform.up);
+                RaycastHit hitInfo;
+                if (Physics.Raycast(ray, out hitInfo))
                 {
-                    float size = Random.Range(minTreeSize, maxTreeSize);
+                    if (hitInfo.transform.tag== "Water"
+                        || hitInfo.transform.tag == "Rock"
+                        || hitInfo.transform.tag == "Tree"
+                        || hitInfo.transform.tag == "Chest")
+                    {
+       
+                    }
+                    else
+                    {
+                        if (randomRotFirst)
+                        {
+                            Instantiate(firstObject, spawnPoint, Quaternion.Euler(0, Random.Range(0, 360), 0), transform.parent);
+                        }
+                        else
+                        {
+                            Instantiate(firstObject, spawnPoint, Quaternion.identity, transform.parent);
+                        }
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < secondAmount; i++)
+        {
+            if (Chance())
+            {
+                Vector3 spawnPoint = new Vector3(Random.Range(firstPos.position.x, secondPos.position.x), spawnHeight, Random.Range(firstPos.position.z, secondPos.position.z));
+                Ray ray = new Ray(spawnPoint, -transform.up);
+                RaycastHit hitInfo;
+                if (Physics.Raycast(ray, out hitInfo))
+                {
+                    if (hitInfo.transform.tag == "Water"
+                        || hitInfo.transform.tag == "Rock"
+                        || hitInfo.transform.tag == "Tree"
+                        || hitInfo.transform.tag == "Chest")
+                    {
 
-                    GameObject newTree = Instantiate(tree);
-                    newTree.transform.localScale = Vector3.one * size;
-                    newTree.transform.position = new Vector3(x, 0, y);
-                    newTree.transform.parent = transform;
+                    }
+                    else
+                    {
+                        if (randomRotSecond)
+                        {
+                            Instantiate(secondObject, spawnPoint, Quaternion.Euler(0, Random.Range(0, 360), 0), transform.parent);
+                        }
+                        else
+                        {
+                            Instantiate(secondObject, spawnPoint, Quaternion.identity,transform.parent);
+                        }
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < thirdAmount; i++)
+        {
+            if (Chance())
+            {
+                Vector3 spawnPoint = new Vector3(Random.Range(firstPos.position.x, secondPos.position.x), spawnHeight, Random.Range(firstPos.position.z, secondPos.position.z));
+                Ray ray = new Ray(spawnPoint, -transform.up);
+                RaycastHit hitInfo;
+                if (Physics.Raycast(ray, out hitInfo))
+                {
+                    if (hitInfo.transform.tag == "Water"
+                        || hitInfo.transform.tag == "Rock"
+                        || hitInfo.transform.tag == "Tree"
+                        || hitInfo.transform.tag == "Chest")
+                    {
+
+                    }
+                    else
+                    {
+                        if (randomRotThird)
+                        {
+                            Instantiate(thirdObject, spawnPoint, Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360)), transform.parent);
+                        }
+                        else
+                        {
+                            Instantiate(thirdObject, spawnPoint, Quaternion.Euler(0, Random.Range(0, 360), 0), transform.parent);
+                        }
+                    }
                 }
             }
         }
     }
-
-    //Returns true or false given some chance from 0 to 1
-    public bool ShouldPlaceTree(float chance)
+    public bool Chance()
     {
-        if (Random.Range(0.0f, 1.0f) <= chance)
+        if (Random.Range(0.00f,5.00f) <= 4.00f)
         {
             return true;
         }
-        return false;
+        else
+        {
+            return false;
+        }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //public GameObject mesh;
-    //[Space(5)]
-    //public float maxTerrainheight;
-    //public float minTerrainheight;
-    //[Space(5)]
-    //public float lastNoiseHeight;
-
-    //[Header("Prefabs")]
-    //public GameObject[] trees;
-    //public GameObject[] rocks;
-    //public GameObject[] chests;
-    //private void MapEmbellishments(float noiseHeight, int posX, int posZ)
-    //{
-    //    Set min and max height of map for color gradient
-
-
-    //    if (System.Math.Abs(lastNoiseHeight - noiseHeight) < 25)
-    //        {
-    //            Generate trees on vertices and times by scale of map
-    //        var bottomOfTile = posX * mesh.transform.localScale.x - 25;
-    //            var topOfTile = posX * mesh.transform.localScale.x + 25;
-    //            var rightOfTile = posZ * mesh.transform.localScale.x - 25;
-    //            var leftOfTile = posZ * mesh.transform.localScale.x + 25;
-
-    //            var spawnAboveTerrainBy = noiseHeight + 25;
-
-    //            for (var i = 0; i < 4; i++)
-    //            {
-    //                if (Random.Range(1, 4) == 1)
-    //                {
-    //                    GameObject objectToSpawn;
-    //                    if (Random.Range(1, 5) == 1)
-    //                    {
-    //                        objectToSpawn = rocks[Random.Range(0, rocks.Length)];
-    //                    }
-    //                    else if (Random.Range(1, 5) == 2)
-    //                    {
-    //                        objectToSpawn = chests[Random.Range(0, chests.Length)];
-    //                    }
-    //                    else
-    //                    {
-    //                        objectToSpawn = trees[Random.Range(0, trees.Length)];
-    //                    }
-
-    //                    switch (i)
-    //                    {
-    //                        case 0:
-    //                            Instantiate(objectToSpawn, new Vector3(topOfTile, spawnAboveTerrainBy, leftOfTile), Quaternion.identity);
-    //                            break;
-    //                        case 1:
-    //                            Instantiate(objectToSpawn, new Vector3(topOfTile, spawnAboveTerrainBy, rightOfTile), Quaternion.identity);
-    //                            break;
-    //                        case 2:
-    //                            Instantiate(objectToSpawn, new Vector3(bottomOfTile, spawnAboveTerrainBy, leftOfTile), Quaternion.identity);
-    //                            break;
-    //                        case 3:
-    //                            Instantiate(objectToSpawn, new Vector3(bottomOfTile, spawnAboveTerrainBy, rightOfTile), Quaternion.identity);
-    //                            break;
-    //                        default:
-    //                            break;
-    //                    }
-    //                }
-    //            }
-
-    //        }
-    //    lastNoiseHeight = noiseHeight;
-    //}
