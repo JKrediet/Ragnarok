dab892ab501048875b759d91538bfc150c45a6e6
ATkingma <keizerxjwz4@gmail.com>
2021-09-01 13:20:30 +0200

    beter to use enviorment spawner

--- Regnarok/Assets/Scripts/map gen/EnviromentSpawner.cs
+++ Regnarok/Assets/Scripts/map gen/EnviromentSpawner.cs
@@ -5,27 +5,12 @@ public class EnviromentSpawner : MonoBehaviour
 {
     public bool testing;
     public bool testEnvSpawn;
-    [Header("Prefabs")]
-    public GameObject grass;
-    public GameObject trees;
-    public GameObject rocks;
-    public GameObject chest;
-    public GameObject totem;
+    public Objects[] spawnItems;
     [Header("Settings")]
     public Transform firstPos;
     public Transform secondPos;
-    [Space(5)]
-    public bool randomRotTrees;
-    public bool randomRotRocks;
-    public bool randomRotchest;
-    public bool randomRotTotem;
-    [Space(5)]
-    public int grassAmount;
-    public int treeAmount;
-    public int rockAmount;
-    public int chestAmount;
-    public int totemAmount;
-    public float spawnHeight = 20;
+    public Transform firstPosInner;
+    public Transform secondPosInner;
     [Space(2)]
     public int sandIndex=0;
     [Space(2)]
@@ -51,181 +36,90 @@ public class EnviromentSpawner : MonoBehaviour
     }
     public void Generate()
     {
-        for (int i = 0; i < grassAmount; i++)
-        {
-            if (Chance())
-            {
-                Vector3 spawnPoint = new Vector3(Random.Range(firstPos.position.x, secondPos.position.x), spawnHeight, Random.Range(firstPos.position.z, secondPos.position.z));
-                Ray ray = new Ray(spawnPoint, -transform.up);
-                RaycastHit hitInfo;
-                if (Physics.Raycast(ray, out hitInfo))
+		for (int i = 0; i < spawnItems.Length; i++)
+		{
+			for (int i_ = 0; i_ < spawnItems[i].amountToSpawn; i_++)
+			{
+                if (Chance())
                 {
-
-                    //checking collor
-                    Renderer renderer = hitInfo.collider.GetComponent<MeshRenderer>();
-                    Texture2D texture2D = renderer.material.mainTexture as Texture2D;
-                    if (texture2D != null)
+                    Vector3 spawnPoint = new Vector3(Random.Range(firstPos.position.x, secondPos.position.x), spawnItems[i].startHeight, Random.Range(firstPos.position.z, secondPos.position.z));
+                    Ray ray = new Ray(spawnPoint, -transform.up);
+                    RaycastHit hitInfo;
+                    if (Physics.Raycast(ray, out hitInfo))
                     {
-                        Vector2 pCoord = hitInfo.textureCoord;
-                        pCoord.x *= texture2D.width;
-                        pCoord.y *= texture2D.height;
-                        Vector2 tiling = renderer.material.mainTextureScale;
-                        Color color = texture2D.GetPixel(Mathf.FloorToInt(pCoord.x * tiling.x), Mathf.FloorToInt(pCoord.y * tiling.y));
 
+                        //checking collor
+                        
 
                         if (hitInfo.transform.tag == "Water"
-                            || color == mapGen.regions[sandIndex].colour)
-                        {
-
-                        }
-                        else
-                        {
-                          
-                            Instantiate(grass, spawnPoint, Quaternion.FromToRotation(Vector3.up, hitInfo.normal) , transform.parent);
-                        }
-                    }
-                }
-            }
-        }
-        for (int i = 0; i < treeAmount; i++)
-        {
-            if (Chance())
-            {
-                Vector3 spawnPoint = new Vector3(Random.Range(firstPos.position.x, secondPos.position.x), spawnHeight, Random.Range(firstPos.position.z, secondPos.position.z));
-                Ray ray = new Ray(spawnPoint, -transform.up);
-                RaycastHit hitInfo;
-                if (Physics.Raycast(ray, out hitInfo))
-                {
-
-                    //checking collor
-                    Renderer renderer = hitInfo.collider.GetComponent<MeshRenderer>();
-                    Texture2D texture2D = renderer.material.mainTexture as Texture2D;
-                    if (texture2D != null)
-                    {
-                        Vector2 pCoord = hitInfo.textureCoord;
-                        pCoord.x *= texture2D.width;
-                        pCoord.y *= texture2D.height;
-                        Vector2 tiling = renderer.material.mainTextureScale;
-                        Color color = texture2D.GetPixel(Mathf.FloorToInt(pCoord.x * tiling.x), Mathf.FloorToInt(pCoord.y * tiling.y));
-                    
-                    
-                        if (hitInfo.transform.tag== "Water"
-                            || hitInfo.transform.tag == "Rock"
-                            || hitInfo.transform.tag == "Tree"
-                            || hitInfo.transform.tag == "Chest"
-                            || hitInfo.transform.tag == "Totem"
-                            || color == mapGen.regions[sandIndex].colour)
-                        {
-       
-                        }
-                    else
-                    {
-                        if (randomRotTrees)
-                        {
-                            Instantiate(trees, spawnPoint, Quaternion.Euler(0, Random.Range(0, 360), 0), transform.parent);
-                        }
-                        else
-                        {
-                            Instantiate(trees, spawnPoint, Quaternion.identity, transform.parent);
-                        }
-                    }
-                    }
-                }
-            }
-        }
-        for (int i = 0; i < rockAmount; i++)
-        {
-            if (Chance())
-            {
-                Vector3 spawnPoint = new Vector3(Random.Range(firstPos.position.x, secondPos.position.x), spawnHeight, Random.Range(firstPos.position.z, secondPos.position.z));
-                Ray ray = new Ray(spawnPoint, -transform.up);
-                RaycastHit hitInfo;
-                if (Physics.Raycast(ray, out hitInfo))
-                {
-                    if (hitInfo.transform.tag == "Water"
                         || hitInfo.transform.tag == "Rock"
                         || hitInfo.transform.tag == "Tree"
                         || hitInfo.transform.tag == "Chest"
                         || hitInfo.transform.tag == "Totem")
-                    {
-
-                    }
-                    else
-                    {
-                        if (randomRotRocks)
                         {
-                            Instantiate(rocks, spawnPoint, Quaternion.Euler(0, Random.Range(0, 360), 0), transform.parent);
+
                         }
                         else
                         {
-                            Instantiate(rocks, spawnPoint, Quaternion.identity,transform.parent);
-                        }
-                    }
-                }
-            }
-        }
-        for (int i = 0; i < chestAmount; i++)
-        {
-            if (Chance())
-            {
-                Vector3 spawnPoint = new Vector3(Random.Range(firstPos.position.x, secondPos.position.x), spawnHeight, Random.Range(firstPos.position.z, secondPos.position.z));
-                Ray ray = new Ray(spawnPoint, -transform.up);
-                RaycastHit hitInfo;
-                if (Physics.Raycast(ray, out hitInfo))
-                {
-                    if (hitInfo.transform.tag == "Water"
-                        || hitInfo.transform.tag == "Rock"
-                        || hitInfo.transform.tag == "Tree"
-                        || hitInfo.transform.tag == "Chest"
-                        || hitInfo.transform.tag == "Totem")
-                    {
+							if (spawnItems[i].canSpawnOnSand)
+							{
+                                if (spawnItems[i].randomRot)
+                                {
+                                    Instantiate(spawnItems[i].toSpawn, spawnPoint, Quaternion.Euler(0, Random.Range(0, 360), 0), transform);
+                                }
+                                else
+                                {
+                                    if (spawnItems[i].rotateWithMesh)
+                                    {
+                                        Instantiate(spawnItems[i].toSpawn, spawnPoint, Quaternion.FromToRotation(Vector3.up, hitInfo.normal), transform);
+                                    }
+									else
+									{
+                                        Instantiate(spawnItems[i].toSpawn, spawnPoint, Quaternion.identity, transform);
+                                    }
+                                }
+                            }
+							else
+							{
+                                Renderer renderer = hitInfo.collider.GetComponent<MeshRenderer>();
+                                Texture2D texture2D = renderer.material.mainTexture as Texture2D;
+                                if (texture2D != null)
+                                {
+                                    Vector2 pCoord = hitInfo.textureCoord;
+                                    pCoord.x *= texture2D.width;
+                                    pCoord.y *= texture2D.height;
+                                    Vector2 tiling = renderer.material.mainTextureScale;
+                                    Color color = texture2D.GetPixel(Mathf.FloorToInt(pCoord.x * tiling.x), Mathf.FloorToInt(pCoord.y * tiling.y));
 
-                    }
-                    else
-                    {
-                        if (randomRotchest)
-                        {
-                            Instantiate(chest, spawnPoint, Quaternion.Euler(0, Random.Range(0, 360), 0), transform.parent);
-                        }
-                        else
-                        {
-                            Instantiate(chest, spawnPoint, Quaternion.Euler(0, 0, 0), transform.parent);
-                        }
-                    }
-                }
-            }
-        }
-        for (int i = 0; i < totemAmount; i++)
-        {
-            if (Chance())
-            {
-                Vector3 spawnPoint = new Vector3(Random.Range(firstPos.position.x, secondPos.position.x), spawnHeight, Random.Range(firstPos.position.z, secondPos.position.z));
-                Ray ray = new Ray(spawnPoint, -transform.up);
-                RaycastHit hitInfo;
-                if (Physics.Raycast(ray, out hitInfo))
-                {
-                    if (hitInfo.transform.tag == "Water"
-                        || hitInfo.transform.tag == "Rock"
-                        || hitInfo.transform.tag == "Tree"
-                        || hitInfo.transform.tag == "Chest"
-                        || hitInfo.transform.tag == "Totem")
-                    {
+                                    if (color == mapGen.regions[sandIndex].colour)
+								    {
 
-                    }
-                    else
-                    {
-                        if (randomRotTotem)
-                        {
-                            Instantiate(totem, spawnPoint, Quaternion.Euler(0, Random.Range(0, 360), 0), transform.parent);
-                        }
-                        else
-                        {
-                            Instantiate(totem, spawnPoint, Quaternion.Euler(0, 0, 0), transform.parent);
-                        }
+								    }
+								    else
+								    {
+                                        if (spawnItems[i].randomRot)
+                                        {
+                                            Instantiate(spawnItems[i].toSpawn, spawnPoint, Quaternion.Euler(0, Random.Range(0, 360), 0), transform);
+                                        }
+                                        else
+                                        {
+                                            if (spawnItems[i].rotateWithMesh)
+                                            {
+                                                Instantiate(spawnItems[i].toSpawn, spawnPoint, Quaternion.FromToRotation(Vector3.up, hitInfo.normal), transform);
+                                            }
+                                            else
+                                            {
+                                                Instantiate(spawnItems[i].toSpawn, spawnPoint, Quaternion.identity, transform);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                         }   
                     }
                 }
             }
-        }
+		}
     }
     public bool Chance()
     {
@@ -237,5 +131,15 @@ public class EnviromentSpawner : MonoBehaviour
         {
             return false;
         }
+    }
+    [System.Serializable]
+    public struct Objects
+    {
+        public GameObject toSpawn;
+        public float startHeight;
+        public int amountToSpawn;
+        public bool canSpawnOnSand;
+        public bool randomRot;
+        public bool rotateWithMesh;
     }
 }
