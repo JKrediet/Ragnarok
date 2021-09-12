0757ebfc97dc6ae3e01874514b8399358fc1aced
ATkingma <60423088+ATkingma@users.noreply.github.com>
2021-09-12 15:10:17 +0200

    totem update

--- Regnarok/Assets/Scripts/prob scripts/Totem.cs
+++ Regnarok/Assets/Scripts/prob scripts/Totem.cs
@@ -6,9 +6,12 @@ public class Totem : MonoBehaviour
 {
     public bool activated;
     public int amountOfEnemies;
+	public Vector3 spawnOffset = new Vector3(0,1,0);
+	public GameObject[] torches;
     private GameScaler gs;
 	public EnemyList enemielist;
 	private int enemiesToSpawn;
+	private List<GameObject> enemies;
 	private void Start()
 	{
 		gs = FindObjectOfType<GameScaler>();
@@ -25,14 +28,36 @@ public class Totem : MonoBehaviour
 		}
 	}
     void SpawnEnemies()
-    {  
+    {
 		for (int i = 0; i < enemiesToSpawn; i++)
 		{
 			int randomNum = Random.Range(0, enemielist.enemieList.Count);
 			Vector3 pos = transform.position;
 			pos.x += Random.Range(0.5f, 3);
 			pos.z += Random.Range(0.5f, 3);
-			Instantiate(enemielist.enemieList[randomNum], pos, Quaternion.identity);
+
+			pos.y = 100;
+			Ray ray = new Ray(pos, -transform.up);
+			RaycastHit hitInfo;
+			if (Physics.Raycast(ray, out hitInfo))
+			{
+				Instantiate(enemielist.enemieList[randomNum], hitInfo.point+spawnOffset, Quaternion.identity);
+			}
 		}
     }
+	public void TotemCheck()
+	{
+		for (int i = 0; i < enemies.Count; i++)
+		{
+			if (enemies[i].GetComponent<Health>())
+			{
+				
+			}
+			else
+			{
+				//kijken of de enemie nog leeft zo niet dan remove je die van de lijst
+				//
+			}
+		}
+	}
 }
