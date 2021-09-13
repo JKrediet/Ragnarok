b8e0620cae51da5a5b17bfb9945d9de801120f9a
ATkingma <60423088+ATkingma@users.noreply.github.com>
2021-09-13 16:46:33 +0200

    totem start

--- Regnarok/Assets/Scripts/prob scripts/Totem.cs
+++ Regnarok/Assets/Scripts/prob scripts/Totem.cs
@@ -4,62 +4,52 @@ using UnityEngine;
 
 public class Totem : MonoBehaviour
 {
-    public bool activated;
-    public int amountOfEnemies;
-	public Vector3 spawnOffset = new Vector3(0,1,0);
+	public bool activated;
+	public int amountOfEnemies;
+	public Vector3 spawnOffset = new Vector3(0, 1, 0);
 	public GameObject[] torches;
-    private GameScaler gs;
 	public EnemyList enemielist;
-	private int enemiesToSpawn;
 	private List<GameObject> enemies;
-	private void Start()
-	{
-		//gs = FindObjectOfType<GameScaler>();
-		//float amountOfEnemies_ = (float)amountOfEnemies;
-		//float resault = (amountOfEnemies_ * gs.scaling);
-		//enemiesToSpawn = (int)resault;
-	}
 	public void Interact()
 	{
+		if (!activated)
+		{
+			SpawnEnemies();
+			activated = true;
+		}
 		print("totem");
-		//if (!isActiveAndEnabled)
-		//{
-  //          SpawnEnemies();
-		//	print("totem");
-  //          activated = true;
-		//}
 	}
-    void SpawnEnemies()
-    {
-		for (int i = 0; i < enemiesToSpawn; i++)
+	void SpawnEnemies()
+	{
+		for (int i = 0; i < amountOfEnemies; i++)
 		{
 			int randomNum = Random.Range(0, enemielist.enemieList.Count);
-			Vector3 pos = transform.position;
-			pos.x += Random.Range(-3.00f, 3.00f);
-			pos.z += Random.Range(-3.00f, 3.00f);
-
-			pos.y = 100;
-			Ray ray = new Ray(pos, -transform.up);
-			RaycastHit hitInfo;
-			if (Physics.Raycast(ray, out hitInfo))
-			{
-				Instantiate(enemielist.enemieList[randomNum], hitInfo.point+spawnOffset, Quaternion.identity);
-			}
+			Vector3 spawnPos = GetSpawnPos();
+			GameObject spawnedEnemie = Instantiate(enemielist.enemieList[randomNum], spawnPos + spawnOffset, Quaternion.identity);
+			enemies.Add(spawnedEnemie);
 		}
-    }
-	public void TotemCheck()
+	}
+	public Vector3 GetSpawnPos()
 	{
-		for (int i = 0; i < enemies.Count; i++)
+		Vector3 pos = transform.position;
+		pos.x += Random.Range(-3.00f, 3.00f);
+		pos.z += Random.Range(-3.00f, 3.00f);
+
+		pos.y = 100;
+		Ray ray = new Ray(pos, -transform.up);
+		RaycastHit hitInfo;
+		if (Physics.Raycast(ray, out hitInfo))
 		{
-			if (enemies[i].GetComponent<Health>())
+			if (hitInfo.transform.tag == "Mesh")
 			{
-				
+
+				return hitInfo.point;
 			}
 			else
 			{
-				//kijken of de enemie nog leeft zo niet dan remove je die van de lijst
-				//
+				GetSpawnPos();
 			}
 		}
+		return new Vector3();
 	}
 }
