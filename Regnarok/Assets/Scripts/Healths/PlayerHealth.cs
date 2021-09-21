807cd8ae930961b49de83d2fa3db50214f9f86d9
ATkingma <keizerxjwz4@gmail.com>
2021-09-21 19:40:33 +0200

    Update PlayerHealth

--- Regnarok/Assets/Scripts/Healths/PlayerHealth.cs
+++ Regnarok/Assets/Scripts/Healths/PlayerHealth.cs
@@ -9,7 +9,13 @@ using UnityEngine.UI;
 public class PlayerHealth : Health
 {
     public Slider HealthSlider;
-
+    public float camRotSpeed;
+    public float respawnTime=15;
+    public GameObject mainCam;
+	public GameObject deathCam;
+    private List<GameObject> players;
+	public bool isDeath;
+    private int camIndex;
     private void Start()
     {
         if (PV.IsMine)
@@ -17,9 +23,26 @@ public class PlayerHealth : Health
             HealthSlider.maxValue = maxHealth;
             HealthSlider.value = health;
             StartCoroutine("HealthRegen");
+            deathCam.SetActive(false);
+            players = new List<GameObject>(GameObject.FindGameObjectsWithTag("Player"));
+
+            for (int i = 0; i < players.Count; i++)
+			{
+				if (players[i] == gameObject)
+				{
+                    players.Remove(players[i]);
+				}
+			}
         }
     }
-    public override void Health_Damage(float damageValue, bool bleed)
+	private void Update()
+	{
+		if (isDeath)
+		{
+            deathCam.transform.Rotate(0, camRotSpeed * Time.deltaTime, 0);
+		}
+	}
+	public override void Health_Damage(float damageValue, bool bleed)
     {
         base.Health_Damage(damageValue, bleed);
         if(HealthSlider)
@@ -43,11 +66,46 @@ public class PlayerHealth : Health
             HealthSlider.maxValue = maxHealth;
         }
     }
+	public override void Health_Dead()
+	{
+        StartCoroutine(RespawnPlayer());
 
-    IEnumerator HealthRegen()
+    }
+	IEnumerator HealthRegen()
     {
         Health_Heal(healthRegen);
         yield return new WaitForSeconds(1);
         StartCoroutine("HealthRegen");
+    }
+    IEnumerator RespawnPlayer()
+	{
+        players[camIndex].GetComponent<PlayerHealth>().deathCam.SetActive(true);
+        mainCam.SetActive(false);
+        gameObject.SetActive(false);
+        isDeath = true;
+        yield return new WaitForSeconds(respawnTime);
+        gameObject.SetActive(true);
+        mainCam.SetActive(true);
+        players[camIndex].GetComponent<PlayerHealth>().deathCam.SetActive(false);
+        health = 100;
+        SincHealthOnMAster(health, false);
+        isDeath = false;
+    }
+    public void CamButton(int i)
+	{
+        players[camIndex].GetComponent<PlayerHealth>().deathCam.SetActive(false);
+        if (camIndex<players.Count&&camIndex>0)
+		{          
+            camIndex += i;
+		}
+        else if (camIndex>players.Count)
+		{
+            camIndex = 0;
+		}
+        else if (camIndex<0)
+		{
+            camIndex = players.Count;
+		}
+        players[camIndex].GetComponent<PlayerHealth>().deathCam.SetActive(true);
     }
 }
