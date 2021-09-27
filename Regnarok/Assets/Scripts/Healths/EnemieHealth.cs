e61044ccb68262e0bb39cdcd0fa656a1a8e9040e
ATkingma <keizerxjwz4@gmail.com>
2021-09-27 22:50:52 +0200

    enemies kunnen dede

--- Regnarok/Assets/Scripts/Healths/EnemieHealth.cs
+++ Regnarok/Assets/Scripts/Healths/EnemieHealth.cs
@@ -1,23 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using UnityEngine.AI;
 
 public class EnemieHealth : Health
 {
 	public int coinDrop;
+	public GameObject healthbar;
 	private GameManager gm;
 	private StateManager sm;
+	private PhotonView pv;
 	public void Start()
 	{
 		gm = FindObjectOfType<GameManager>();
 		sm = GetComponent<StateManager>();
+		pv = GetComponent<PhotonView>();
 	}
 	public override void Health_Dead()
 	{
-		RollItem();
-		GiveXP();
-		DropMoney();
 		sm.isDead = true;
+		sm.ResetAnim();
+		sm.anim.SetBool("IsDying", true);
+		Destroy(gameObject.GetComponent<NavMeshAgent>());
+		Destroy(gameObject.GetComponent<NavMeshObstacle>());
+		Destroy(healthbar);
 	}
 	public void RollItem()
 	{
@@ -31,5 +38,12 @@ public class EnemieHealth : Health
 	{
 		coinDrop *= (gm.days / 100);
 		//instantiate hierro
+	}
+	public void GivePlayerStuff()
+	{
+		RollItem();
+		GiveXP();
+		DropMoney();
+		PhotonNetwork.Destroy(gameObject);
 	}
 }
