5c521b171fbfaacf095b48730886895b557b0fb1
ATkingma <60423088+ATkingma@users.noreply.github.com>
2021-10-04 15:47:56 +0200

    printjes

--- Regnarok/Assets/SeeOtherPlayerScript.cs
+++ Regnarok/Assets/SeeOtherPlayerScript.cs
@@ -19,22 +19,27 @@ public class SeeOtherPlayerScript : MonoBehaviour
 	}
 	private void Update()
 	{
-		if (Input.GetButtonDown("H"))
+		if (Input.GetKeyDown(KeyCode.H)
 		{
+			print("1");
 			if (players.Count <= 0)
 			{
+				print("5");
 				players = new List<SeeOtherPlayerScript>(FindObjectsOfType<SeeOtherPlayerScript>());
 				for (int i = 0; i < players.Count; i++)
 				{
+					print("4");
 					if (players[i]==this)
 					{
 						players.Remove(players[i]);
+						print("3");
 					}
 				}
 			}
 			for (int i = 0; i < players.Count; i++)
 			{
 				Trigger(i);
+				print("2");
 			}
 		}
 	}
