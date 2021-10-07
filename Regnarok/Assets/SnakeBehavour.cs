2f8de2e7b77ec60c357b9f8a68813c513c7f7df7
ATkingma <keizerxjwz4@gmail.com>
2021-10-08 01:39:44 +0200

    laptop push

--- Regnarok/Assets/SnakeBehavour.cs
+++ Regnarok/Assets/SnakeBehavour.cs
@@ -7,19 +7,30 @@ public class SnakeBehavour : MonoBehaviour
     public List<GameObject> targets;
     public float attackTime;
 	public float targetCooldown;
+	public ParticleSystem ps;
 	private bool isGettingTarget;
+	private bool isAttacking;
 	private GameObject target;
-    void Start()
-    {
-        
-    }
-    void Update()
-    {
-		if (!isGettingTarget)
-		{
-			StartCoroutine(GetTarget());
-		}
-    }
+	private Animator anim;
+	private void Start()
+	{
+		anim = GetComponent<Animator>();
+		ps.Play();
+	}
+	//void Update()
+ //   {
+	//	if (!isGettingTarget)
+	//	{
+	//		StartCoroutine(GetTarget());
+	//	}
+	//	if (!isAttacking)
+	//	{
+	//		if (target != null)
+	//		{
+	//			StartCoroutine(Attack());
+	//		}
+	//	}
+ //   }
 	private void OnTriggerEnter(Collider other)
 	{
 		if (other.CompareTag("Enemy"))
@@ -42,7 +53,7 @@ public class SnakeBehavour : MonoBehaviour
 			if (targets[i] != null)
 			{
 				float dis = Vector3.Distance(targets[i].transform.position, transform.position);
-				if(dis< Vector3.Distance(target.transform.position, transform.position))
+				if (dis < Vector3.Distance(target.transform.position, transform.position))
 				{
 					target = targets[i];
 				}
@@ -58,5 +69,11 @@ public class SnakeBehavour : MonoBehaviour
 		}
 		yield return new WaitForSeconds(targetCooldown);
 		isGettingTarget = false;
+	}
+	public IEnumerator Attack()
+	{
+		isAttacking = true;
+		yield return new WaitForSeconds(attackTime);
+		isAttacking = false;
 	}
 }
